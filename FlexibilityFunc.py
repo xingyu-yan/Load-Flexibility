94d45edca4f26c062d3fcdfa2085adce80a52f5d
Xingyu YAN <xingyu-yan@outlook.com>
2020-04-27 10:19:42 +0800

    Update FlexibilityFunc.py

--- FlexibilityFunc.py
+++ FlexibilityFunc.py
@@ -26,16 +26,16 @@ def FlexibilityCalculate(Load, Load_Name):
     print('Daily Flexibility of ' + Load_Name + ' Is %d MWh' % Flexi_Day)
     
     # plot the daily flexibility
-    
-    Load_Mat_Dif_toList_ThreeDays = np.reshape(np.mat(Load_Mat_Dif[0:3,:]), (1, -1)).tolist()
-    x = Load[0:72]
-    y = np.array(Load[0:72]) - np.array(Load_Mat_Dif_toList_ThreeDays[0])
+    NumDayPlot = 4  # The number of consecutive days to plot
+    Load_Mat_Dif_toList_Days = np.reshape(np.mat(Load_Mat_Dif[0:NumDayPlot,:]), (1, -1)).tolist()
+    x = Load[0:(24*NumDayPlot)]
+    y = np.array(Load[0:(24*NumDayPlot)]) - np.array(Load_Mat_Dif_toList_Days[0])
     plt.figure()
     plt.plot(x, label="Load")
     plt.plot(y, label="Daily Average Load")
-    # plt.plot(np.abs(Load_Mat_Dif_toList_ThreeDays[0]), label="Daily Flexibility")
+    # plt.plot(np.abs(Load_Mat_Dif_toList_Days[0]), label="Daily Flexibility")
     plt.tight_layout()
-    plt.fill_between(range(72), x, y, facecolor="orange", # The fill color
+    plt.fill_between(range(24*NumDayPlot), x, y, facecolor="orange", # The fill color
                      color='blue',       # The outline color
                      alpha=0.2)                 
     plt.title('Daily Load Flexibility of ' + Load_Name)
@@ -48,10 +48,10 @@ def FlexibilityCalculate(Load, Load_Name):
     # 周波动性计算
     # 1计算每周平均值
     print('Calculating the Weekly Load Flexibility ...')
-    Load_Mat_Week = np.reshape(np.mat(Load[0:8736]), (-1, 168))
+    Load_Mat_Week = np.reshape(np.mat(Load[0:int(len(Load)/(24*7))*(24*7)]), (-1, 168))
 
     AVE_Week = np.mean(Load_Mat_Week, axis=1)
-    print('The Data Shape of Average Weekly Load Is '  + str(AVE_Week.shape))
+    # print('The Data Shape of Average Weekly Load Is '  + str(AVE_Week.shape))
 
     # 2计算每周中每天的波动性
     # 每天平均值
@@ -63,16 +63,17 @@ def FlexibilityCalculate(Load, Load_Name):
     print('Weekly Flexibility of ' + Load_Name + ' Is %d MWh' % Flexi_Week)    
     
     # plot the daily flexibility
-    Load_Mat_Week_Day_toList_3Weeks = np.reshape(np.mat(Load_Mat_Week_Day[0:3,:]), (1, -1)).tolist()
-    Load_Mat_Week_Day_Dif_toList_3Weeks = np.reshape(np.mat(Load_Mat_Week_Day_Dif[0:3,:]), (1, -1)).tolist()
-    x = Load_Mat_Week_Day_toList_3Weeks[0]
-    y = Load_Mat_Week_Day_toList_3Weeks[0] - np.array(Load_Mat_Week_Day_Dif_toList_3Weeks[0])
+    NumWeekPlot = 4  # The number of consecutive weeks to plot
+    Load_Mat_Week_Day_toList_Weeks = np.reshape(np.mat(Load_Mat_Week_Day[0:NumWeekPlot,:]), (1, -1)).tolist()
+    Load_Mat_Week_Day_Dif_toList_Weeks = np.reshape(np.mat(Load_Mat_Week_Day_Dif[0:NumWeekPlot,:]), (1, -1)).tolist()
+    x = Load_Mat_Week_Day_toList_Weeks[0]
+    y = Load_Mat_Week_Day_toList_Weeks[0] - np.array(Load_Mat_Week_Day_Dif_toList_Weeks[0])
     plt.figure()
     plt.plot(x, label="Average Daily Load")
     plt.plot(y, label="Weekly Average Load")
-    # plt.plot(np.abs(Load_Mat_Week_Day_Dif_toList_3Weeks[0]), label="Weekly Flexibility")
+    # plt.plot(np.abs(Load_Mat_Week_Day_Dif_toList_Weeks[0]), label="Weekly Flexibility")
     plt.tight_layout()
-    plt.fill_between(range(21), x, y, facecolor="orange", # The fill color
+    plt.fill_between(range(7*NumWeekPlot), x, y, facecolor="orange", # The fill color
                      color='blue',       # The outline color
                      alpha=0.2)
     plt.title('Weekly Load Flexibility of ' + Load_Name)
