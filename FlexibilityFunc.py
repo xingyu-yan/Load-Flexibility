2de9e84c582613aaa259800378cebc3a388a115c
Xingyu YAN <xingyu-yan@outlook.com>
2020-05-03 21:18:51 +0800

    Add files via upload

--- FlexibilityFunc.py
+++ FlexibilityFunc.py
@@ -8,10 +8,61 @@ Created on Sun Apr 26 08:28:32 2020
 import numpy as np
 import matplotlib.pyplot as plt
 
+# average daily residual load
+def Ave_Resi_Load(Load, Load_Sub_Name): 
+    print('Calculating the Average Daily Residual Load of ' + Load_Sub_Name +' ...')
+    Load_Mat = np.reshape(Load, (-1, 24))/1000  # /1000: MW to GW
+    
+    # Average daily residual load
+    AVE_Hour = np.reshape(np.mean(Load_Mat, axis=0), (1, -1)).tolist()[0]
+    
+    # Hourly Gradients
+    Hourly_Grad = []
+    for i in range(len(AVE_Hour)):
+        if i == 0:
+            HourlyGrad = AVE_Hour[i] - AVE_Hour[len(AVE_Hour)-1]
+        else:
+            HourlyGrad = AVE_Hour[i] - AVE_Hour[i-1]
+        Hourly_Grad.append(HourlyGrad)
+    
+    # plt.figure()
+    # x = range(len(AVE_Hour))
+    # plt.plot(x, AVE_Hour, label='Average Residual Load')  
+    # plt.plot(x, Hourly_Grad, label='Ramp Rate') 
+    # plt.title('Hourly Average Residual Load and Ramp Rate for ' + Load_Sub_Name)
+    # plt.ylabel('GW')
+    # plt.xlabel('Time (Hour)')
+    # # plt.legend(bbox_to_anchor=(1, 1), loc='upper left', borderaxespad=0.)
+    # plt.legend()
+    # plt.show()
+    
+    fig, ax1 = plt.subplots()
+    x = range(len(AVE_Hour))
+    color = 'tab:red'
+    ax1.set_xlabel('Time (Hour)')
+    ax1.set_ylabel('Average Residual Load (GW)', color=color)
+    ax1.plot(x, AVE_Hour, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+    plt.legend()
+    
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis 
+    color = 'tab:blue'
+    ax2.set_ylabel('Ramp Rate (GWh)', color=color)  # we already handled the x-label with ax1
+    ax2.plot(x, Hourly_Grad, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)    
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.title('Hourly Average Residual Load and Ramp Rate for ' + Load_Sub_Name)
+    plt.legend()
+    plt.show()
+    
+    return AVE_Hour, Hourly_Grad
+    
+    
 # 净负荷波动性计算
 def FlexibilityCalculate(Load, Load_Name):
-    print('Calculating the Daily Load Flexibility ...')
+    print('Calculating the Load Flexibility of ' + Load_Name +' ...')
     # Load_Mat = np.reshape(np.mat(Load), (-1, 24))
+    # Load = Load/1000  # /1000: MW to GW
     Load_Mat = np.reshape(Load, (-1, 24))
     
     # 日波动性计算
@@ -22,24 +73,26 @@ def FlexibilityCalculate(Load, Load_Name):
     Load_Mat_Dif = Load_Mat - AVE_Day
 
     # 3计算日灵活性 Flexibility
-    Flexi_Day = np.abs(Load_Mat_Dif).sum()
-    print('Daily Flexibility of ' + Load_Name + ' Is %d MWh' % Flexi_Day)
+    # Flexi_Day = np.abs(Load_Mat_Dif).sum()
+    Flexi_Day = np.array(np.abs(Load_Mat_Dif).sum())/2/1000/1000 
+    # # /2:只计算平均值上面部分； /1000/1000: MWh to TWh
+    print('Daily Flexibility of ' + Load_Name + ' Is %d TWh' % Flexi_Day)
     
     # plot the daily flexibility
     NumDayPlot = 4  # The number of consecutive days to plot
     Load_Mat_Dif_toList_Days = np.reshape(np.mat(Load_Mat_Dif[0:NumDayPlot,:]), (1, -1)).tolist()
-    x = Load[0:(24*NumDayPlot)]
-    y = np.array(Load[0:(24*NumDayPlot)]) - np.array(Load_Mat_Dif_toList_Days[0])
+    x = np.array(Load[0:(24*NumDayPlot)])/1000
+    y = np.array(Load[0:(24*NumDayPlot)])/1000 - np.array(Load_Mat_Dif_toList_Days[0])/1000
     plt.figure()
-    plt.plot(x, label="Load")
-    plt.plot(y, label="Daily Average Load")
+    plt.plot(x, label="Hourly Load")        # /1000: MW to GW
+    plt.plot(y, label="Daily Average Load") # /1000: MW to GW
     # plt.plot(np.abs(Load_Mat_Dif_toList_Days[0]), label="Daily Flexibility")
     plt.tight_layout()
     plt.fill_between(range(24*NumDayPlot), x, y, facecolor="orange", # The fill color
                      color='blue',       # The outline color
                      alpha=0.2)                 
     plt.title('Daily Load Flexibility of ' + Load_Name)
-    plt.ylabel('MW')
+    plt.ylabel('GW')
     plt.xlabel('Time (Hour)')
     # plt.legend(bbox_to_anchor=(1, 1), loc='upper left', borderaxespad=0.)
     plt.legend()
@@ -47,7 +100,6 @@ def FlexibilityCalculate(Load, Load_Name):
 
     # 周波动性计算
     # 1计算每周平均值
-    print('Calculating the Weekly Load Flexibility ...')
     Load_Mat_Week = np.reshape(np.mat(Load[0:int(len(Load)/(24*7))*(24*7)]), (-1, 168))
 
     AVE_Week = np.mean(Load_Mat_Week, axis=1)
@@ -59,28 +111,69 @@ def FlexibilityCalculate(Load, Load_Name):
     Load_Mat_Week_Day_Dif = Load_Mat_Week_Day - AVE_Week
 
     # 计算周灵活性 Flexibility
-    Flexi_Week = np.abs(Load_Mat_Week_Day_Dif).sum()
-    print('Weekly Flexibility of ' + Load_Name + ' Is %d MWh' % Flexi_Week)    
+    Flexi_Week = np.array(np.abs(Load_Mat_Week_Day_Dif).sum())/2/1000/1000 
+    # /2:只计算平均值上面部分； /1000/1000: MWh to TWh
+    print('Weekly Flexibility of ' + Load_Name + ' Is %d TWh' % Flexi_Week)    
     
     # plot the daily flexibility
     NumWeekPlot = 4  # The number of consecutive weeks to plot
     Load_Mat_Week_Day_toList_Weeks = np.reshape(np.mat(Load_Mat_Week_Day[0:NumWeekPlot,:]), (1, -1)).tolist()
     Load_Mat_Week_Day_Dif_toList_Weeks = np.reshape(np.mat(Load_Mat_Week_Day_Dif[0:NumWeekPlot,:]), (1, -1)).tolist()
-    x = Load_Mat_Week_Day_toList_Weeks[0]
-    y = Load_Mat_Week_Day_toList_Weeks[0] - np.array(Load_Mat_Week_Day_Dif_toList_Weeks[0])
+    x = np.array(Load_Mat_Week_Day_toList_Weeks[0])/1000  # /1000: MW to GW
+    y = np.array(Load_Mat_Week_Day_toList_Weeks[0] - np.array(Load_Mat_Week_Day_Dif_toList_Weeks[0]))/1000
     plt.figure()
-    plt.plot(x, label="Average Daily Load")
-    plt.plot(y, label="Weekly Average Load")
+    plt.plot(x, label="Average Daily Load")  
+    plt.plot(y, label="Weekly Average Load") # /1000: MW to GW
     # plt.plot(np.abs(Load_Mat_Week_Day_Dif_toList_Weeks[0]), label="Weekly Flexibility")
     plt.tight_layout()
     plt.fill_between(range(7*NumWeekPlot), x, y, facecolor="orange", # The fill color
                      color='blue',       # The outline color
                      alpha=0.2)
     plt.title('Weekly Load Flexibility of ' + Load_Name)
-    plt.ylabel('MW')
+    plt.ylabel('GW')
     plt.xlabel('Time (Day)')
     # plt.legend(bbox_to_anchor=(1, 1), loc='upper left', borderaxespad=0.)
     plt.legend()
     plt.show()
     
-    return Flexi_Day, Flexi_Week
+    # 年波动性计算
+    # 1计算剩余负荷月平均值
+    Load_Mat_Month = np.reshape(np.mat(Load[0:len(Load)]), (-1, 730))
+    
+    AVE_Month = np.mean(Load_Mat_Month, axis=1)
+    # print('The Data Shape of Average Weekly Load Is '  + str(AVE_Week.shape))
+
+    # 2计算每年中每月的波动性
+    # 每年平均值
+    AVE_Year = np.mean(Load) 
+    Load_Mat_Year_Month_Dif = AVE_Month - AVE_Year
+
+    # 计算周灵活性 Flexibility
+    Flexi_Year = np.array(np.abs(Load_Mat_Year_Month_Dif).sum())/2/1000
+    # 2:只计算平均值上面部分； /1000: MWh to GWh
+    print('Yearly Flexibility of ' + Load_Name + ' Is %d GWh' % Flexi_Year)    
+    
+    # plot the yearly flexibility
+
+    x1 = np.reshape((np.repeat(AVE_Year, 12))/1000, (1, -1)).tolist()  # /1000: MW to GW
+    x2 = x1[0]
+    Load_Mat_Year_Month_Dif_toList = np.reshape(np.mat(Load_Mat_Year_Month_Dif), (1, -1)).tolist()
+    y1 = x1 - np.array(Load_Mat_Year_Month_Dif_toList[0])
+    y2 = y1[0]
+    
+    plt.figure()
+    plt.plot(x2, label="Annual Average Load")  
+    plt.plot(y2, label="Monthly Average Load") # /1000: MW to GW
+    # plt.plot(np.abs(Load_Mat_Week_Day_Dif_toList_Weeks[0]), label="Weekly Flexibility")
+    # plt.tight_layout()
+    plt.fill_between(range(12), x2, y2, facecolor="orange", # The fill color
+                      color='blue',       # The outline color
+                      alpha=0.2 )
+    plt.title('Yearly Load Flexibility of ' + Load_Name)
+    plt.ylabel('GW')
+    plt.xlabel('Time (Month)')
+    # plt.legend(bbox_to_anchor=(1, 1), loc='upper left', borderaxespad=0.)
+    plt.legend()
+    plt.show()
+       
+    return Flexi_Day, Flexi_Week, Flexi_Year
---
+++ LoadFlexibilityMain3.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 26 09:11:39 2020
+At GEIDCO, Beijing
+@author: Xingyu
+Objective: Load Flexibility Calculation 
+"""
+
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from FlexibilityFunc import FlexibilityCalculate, Ave_Resi_Load
+
+# 读取csv文件
+csvFile = open("HourlyData_Region_2035_NetLoad_OR.csv", "r")
+
+# 方法1
+# reader = csv.DictReader(csvFile) 
+# column = [row['FR'] for row in reader] # 选取法国的数据作为计算对象
+# print('The Column Number of All Load is %d' % len(column))
+# Load = list(map(float, column)) # 字符串转换为数字  
+
+# 方法2
+reader = csv.reader(csvFile) 
+rows = [row for row in reader]
+print('File info: ' + str(rows[0][0]))
+FirstLine = rows[4]
+for index in range(len(FirstLine)):
+    print('The column %d is: ' % index + str(FirstLine[index]))
+
+# Load = np.mat(rows[5:len(rows)])[0:8761,3:16]    
+Load = np.mat(rows[4:len(rows)])[0:8761]
+
+# #Plot the Load of PRC  
+# plt.figure()
+# plt.plot(Load[1:8761,7])
+# # plt.plot(Load[0:168])
+# plt.title('NetLoad profile of PRC')
+# plt.ylabel('MW')
+# plt.xlabel('Time (Hour)')
+# plt.show()
+
+# Country = input('Which Country Do You Want to Choose: ')
+    
+Flexibility_Day = []
+Flexibility_Week = []
+Flexibility_Year = []
+
+Ave_Hourly_Load = []
+Hourly_Graddient = []
+for i in range(13):
+    # 字符串转换为数字
+    if i == 4:
+        print()
+        print('Skip the ' + Load[0,i+3] )
+        print()
+    else:
+        Load_Sub = list(map(float,Load[1:8761,i+3]))
+        Load_Sub_Name = 'Load_' + Load[0,i+3]
+        print('Calculating the Flexibility of ' + Load_Sub_Name)
+        
+        (Flexi_Day, Flexi_Week, Flexi_Year) = FlexibilityCalculate(Load_Sub, Load_Sub_Name)
+        Flexibility_Day.append(Flexi_Day)
+        Flexibility_Week.append(Flexi_Week)
+        Flexibility_Year.append(Flexi_Year)   
+        
+        (AVE_Hour, Hourly_Grad) = Ave_Resi_Load(Load_Sub, Load_Sub_Name)
+        Ave_Hourly_Load.append(AVE_Hour)
+        Hourly_Graddient.append(Hourly_Grad)
+
+    
+# Plot the load flexibility result  
+# plt.figure()
+# # labels = ['GB', 'EPT', 'EUR_SE', 'Central Asia', 'PRC', 'FR', 'BNL', 'DE', 'CH', 'PL', 'SC', 'BL', 'IT']
+# Locations = ['GB', 'EPT', 'EUR_SE', 'Central Asia', 'FR', 'BNL', 'DE', 'CH', 'PL', 'SC', 'BL', 'IT']
+# x = np.arange(len(Locations))
+# width = 0.3
+# plt.bar(x, Flexibility_Day, width, label='Daily Load Flexibility', fc = 'y')
+# plt.bar(x + width, Flexibility_Week, width, label='Weekly Load Flexibility', fc = 'r')
+# plt.bar(x + 2*width, Flexibility_Year, width, label='Yearly Load Flexibility', fc = 'b')
+# plt.xticks(x+width/3, labels, rotation='vertical') # rotation=45
+# plt.title('NetLoad Flexibility')
+# plt.ylabel('TWh')
+# plt.xlabel('Country/Region')
+# plt.legend()
+# plt.show() 
+
+# plt.figure()
+# Locations = ['GB', 'EPT', 'EUR_SE', 'Cent_Asia', 'FR', 'BNL', 'DE', 'CH', 'PL', 'SC', 'BL', 'IT']
+# x = np.arange(len(Locations))
+# plt.subplot(3, 1, 1)
+# plt.bar(x, Flexibility_Day, label='Daily Load Flexibility', fc = 'y')
+# plt.ylabel('TWh')
+# plt.legend()
+# plt.title('NetLoad Flexibility')
+# plt.subplot(3, 1, 2)
+# plt.bar(x, Flexibility_Week, label='Weekly Load Flexibility', fc = 'r')
+# plt.ylabel('TWh')
+# plt.legend()
+# plt.subplot(3, 1, 3)
+# plt.bar(x, Flexibility_Year, label='Yearly Load Flexibility', fc = 'b')
+# plt.ylabel('GWh')
+# plt.legend()
+# plt.xticks(x, Locations, rotation=30) # rotation='vertical'
+# plt.xlabel('Country/Region')
+# plt.show() 
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+Locations = ['GB', 'EPT', 'EUR_SE', 'Cent_Asia', 'FR', 'BNL', 'DE', 'CH', 'PL', 'SC', 'BL', 'IT']
+x = np.arange(len(Locations))
+labels = ['Daily Load Flexibility', 'Weekly Load Flexibility', 'Yearly Load Flexibility']
+fig.suptitle('NetLoad Flexibility')
+ax1.bar(x, Flexibility_Day, color='red', label=labels[0])
+ax1.legend(loc="upper right")
+ax1.set_ylabel('TWh')
+ax2.bar(x, Flexibility_Week, color='orange', label=labels[1])
+ax2.legend(loc="upper left")
+ax2.set_ylabel('TWh')
+ax3.bar(x, Flexibility_Year, color='blue', label=labels[2])
+ax3.legend(loc="upper left")
+ax3.set_ylabel('GWh')
+plt.xticks(x, Locations, rotation='vertical') # rotation='vertical'
+plt.xlabel('Country/Region')
+plt.show() 
+
+# Plot the average daily residual load results
+#Plot the Load of PRC  
+plt.figure()
+label = ['GB', 'EPT', 'EUR_SE', 'Cent_Asia', 'FR', 'BNL', 'DE', 'CH', 'PL', 'SC', 'BL', 'IT']
+for i in range(len(Ave_Hourly_Load)):
+    plt.plot(Ave_Hourly_Load[i], label=label[i])
+# plt.plot(Load[0:168])
+plt.title('Average Residual Load')
+plt.ylabel('GW')
+plt.xlabel('Time (Hour)')
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show()
+
+plt.figure()
+label = ['GB', 'EPT', 'EUR_SE', 'Cent_Asia', 'FR', 'BNL', 'DE', 'CH', 'PL', 'SC', 'BL', 'IT']
+for i in range(len(Ave_Hourly_Load)):
+    plt.plot(Hourly_Graddient[i], label=label[i])
+# plt.plot(Load[0:168])
+plt.title('Average Residual Load Ramp Rate')
+plt.ylabel('GWh')
+plt.xlabel('Time (Hour)')
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show()
+
+print('Congratulations, task completed !') 
